36078fbced325f2c77fc52505ef3c23d19e394c9
John O'Reilly <oreillyj@gmail.com>
2023-04-03 15:08:13 +0100

    talk code updates

--- shared/src/commonMain/kotlin/dev/johnoreilly/confetti/ConfettiRepository.kt
+++ shared/src/commonMain/kotlin/dev/johnoreilly/confetti/ConfettiRepository.kt
@@ -7,6 +7,47 @@ import com.apollographql.apollo3.cache.normalized.sql.SqlNormalizedCacheFactory
 import kotlinx.coroutines.flow.*
 
 
+class ConfettiRepository {
+    val conferenceName = "KotlinConf 2023"
+
+    // Create Apollo client with normalized cache (in memory and persistent)
+    private val apolloClient = ApolloClient.Builder()
+        .serverUrl("http://localhost:8080/graphql")
+        //.serverUrl("https://kotlinconfetti.ew.r.appspot.com/graphql")
+        .normalizedCache(MemoryCacheFactory(10_000_000).chain(SqlNormalizedCacheFactory(getDatabaseName(conferenceName))))
+        .build()
+
+
+    // Gets list of sessions from backend and then observes the cache for any changes
+    val sessions = apolloClient.query(GetSessionsQuery()).watch().map {
+        it.dataAssertNoErrors.sessions.map { it.sessionDetails }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- shared/src/jvmMain/kotlin/Main.kt
+++ shared/src/jvmMain/kotlin/Main.kt
@@ -4,7 +4,15 @@ import dev.johnoreilly.confetti.ConfettiRepository
 
 
 
+suspend fun main() {
+    val repo = ConfettiRepository()
 
+    repo.sessions.collect { sessions ->
+        sessions.forEach { session ->
+            println("${session.start}  ${session.title}")
+        }
+    }
+}
 
 
 
